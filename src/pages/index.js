21b9f6d738aba9121b80bab72a9ec1356b86c6f7
Cindy Juarez <sceendy@gmail.com>
2017-12-29 13:25:38 -0600

    new blog post

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,10 +21,10 @@ const IndexPage = ({data}) => (
           <h1>Hi, I’m Cindy!</h1>
           <h2>I'm a front-end engineer in Texas.</h2>
         </header>
-        <p>I love building empowering, functional, efficient, and thoughtfully designed web applications. I love JavaScript and tend to work remotely most days.</p>
-        <p>There are some great engineers that I have worked with and learned a lot from over the past few years. They have some <a href='https://www.linkedin.com/in/sceendy#recommendations' target='_blank' rel='noopener'>good things to say about me</a>.</p>
-        <p>I live in Austin with my husband and dog. I enjoy running, drinking coffee, eating waffles, and traveling.</p>
-        <a href={resume} className='btn u--float-right' target='_blank'>View Resume (.pdf)</a>
+        <p>I enjoy building empowering, functional, efficient and thoughtfully designed web applications.</p>
+        <p>There are some great engineers that I have worked with over the past few years that have some <a href='https://www.linkedin.com/in/sceendy#recommendations' target='_blank' rel='noopener'>good things to say about me</a>.</p>
+        <p>I live in Austin. I love running, coffee, and traveling.</p>
+        <a href={resume} className='btn u--float-right' target='_blank' title='Resume pdf'>View Resume (.pdf)</a>
       </div>
     </div>
 
@@ -36,9 +36,9 @@ const IndexPage = ({data}) => (
         className='figure__image'
         alt="Screenshot of AMS" />	
         <figcaption>
-          <a href='https://ams.kalkomey.com/' target='_blank' rel='noopener'>Agency Management Solution</a><br/>
+          <a href='https://ams.kalkomey.com/' target='_blank' rel='noopener' title='AMS marketing page; product requires login'>Agency Management Solutions</a><br/>
           <small>SCSS, JavaScript, Ruby on Rails</small>
-          <p>A product suite that provides tools for clients to manage their content, users, and products.</p>
+          <p>A product suite that provides tools for Kalkomey clients to manage their content and users.</p>
         </figcaption>
       </figure>
       <figure>
@@ -47,7 +47,7 @@ const IndexPage = ({data}) => (
         className='figure__image'
         alt="Screenshot of PA Game news" />	
         <figcaption>
-          <a href='https://pennsylvaniagamenews.com/' target='_blank' rel='noopener'>Kalkomey Outdoor Shop</a><br/>
+          <a href='https://pennsylvaniagamenews.com/' target='_blank' rel='noopener' title='Kalkomey Storefront Site for the state of Pennsylvania'>Kalkomey Outdoor Shop</a><br/>
           <small>SCSS, Angular/TypeScript, Node.js</small>
           <p>A customizable storefront for users to purchase products and manage their account.</p>
         </figcaption>
@@ -58,9 +58,9 @@ const IndexPage = ({data}) => (
         className='figure__image'
         alt="Screenshot of Foundry" />	
         <figcaption>
-        <a href='https://kalkomey.com/' target='_blank' rel='noopener'>Foundry CMS</a><br/>
+        <a href='https://kalkomey.com/' target='_blank' rel='noopener' title='Kalkomey marketing website; Foundry requires a login'>Foundry CMS</a><br/>
           <small>SCSS, AngularJS, Ruby on Rails</small>
-          <p>An internal content management system for online outdoor recreational safety courses.</p>
+          <p>An internal content/learning management system for online safety education courses.</p>
         </figcaption>
       </figure>
     </div>
