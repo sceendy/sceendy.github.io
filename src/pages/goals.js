c322495984b2c46f95c33b7ed3fef45a41501e38
Cindy Juarez <sceendy@gmail.com>
2019-02-24 19:08:54 -0600

    updated progress percentage

--- src/pages/goals.js
+++ src/pages/goals.js
@@ -1,33 +1,68 @@
 import React, { Fragment } from 'react';
 
-const GoalsComponent = () => (
-  <Fragment>
-    <h2>Q1 2019 goals</h2>
-    <div className="goal__container">
-      Work on personal project for 20 hours
-      <progress max="20" value="3"></progress>
-    </div>
-    <div className="goal__container">
-      Meditate for 4 hours
-      <progress max="240" value="90"></progress>
-    </div>
-    <div className="goal__container">
-      Run 50 miles
-      <progress max="50" value="15"></progress>
-    </div>
-    <div className="goal__container">
-      Lose 15lbs
-      <progress max="15" value="0"></progress>
-    </div>
-    <div className="goal__container">
-      Complete Udacitys' Intro to CS course
-      <progress max="100" value="52"></progress>
-    </div>
-    <div className="goal__container">
-      Complete Pluralsights' React Path
-      <progress max="100" value="62"></progress>
-    </div>
-  </Fragment>
-);
+const getPercentage = (value, max) =>  `${Math.floor((value/max) * 100)}%`;
+
+const goals = [
+  {
+    goalId: 'personalProject',
+    title: 'Work on personal project for 20 hours',
+    total: 20,
+    completed: 3
+  },
+  {
+    goalId: 'meditate',
+    title: 'Meditate for 4 hours',
+    total: 240,
+    completed: 90
+  },
+  {
+    goalId: 'run',
+    title: 'Run 50 miles',
+    total: 50,
+    completed: 15
+  },
+  {
+    goalId: 'loseWeight',
+    title: 'Lose 15lbs',
+    total: 15,
+    completed: 0
+  },
+  {
+    goalId: 'learning1',
+    title: 'Complete Udacitys\' Intro to CS course',
+    total: 100,
+    completed: 52
+  },
+  {
+    goalId: 'learning2',
+    title: 'Complete Pluralsights\' React Path',
+    total: 100,
+    completed: 65
+  },
+];
+
+const GoalsComponent = () => {
+  const getProgress = (idName) => {
+    const progressBar = document.getElementById(idName);
+    if (progressBar) {
+      const value = progressBar.getAttribute('value');
+      const max = progressBar.getAttribute('max');
+      return getPercentage(value, max);
+    }
+  };
+  
+  return (
+    <Fragment>
+      <h2>Q1 2019 goals</h2>
+      {goals.map(goal =>
+        <div className="goal__container" key={goal.goalId}>
+          {goal.title}
+          <span className="goal__percentage">{getProgress(goal.goalId)}</span>
+          <progress max={goal.total} value={goal.completed} id={goal.goalId}></progress>
+        </div>
+      )}
+    </Fragment>
+  );
+};
 
 export default GoalsComponent;
