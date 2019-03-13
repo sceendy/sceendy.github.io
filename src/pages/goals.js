98926604ced56f9571b73a2f90983d9929260920
Cindy Juarez <sceendy@gmail.com>
2019-03-13 18:41:08 -0500

    refactored goals data; updated goals 03/13

--- src/pages/goals.js
+++ src/pages/goals.js
@@ -3,49 +3,39 @@ import { StaticQuery, graphql } from 'gatsby';
 
 const getPercentage = (value, max) =>  `${Math.floor((value/max) * 100)}%`;
 
-const GoalsComponent = () => {
-  const getProgress = (idName) => {
-    if(typeof document !== "undefined") {
-      const progressBar = document.getElementById(idName);
-      if (progressBar) {
-        const value = progressBar.getAttribute('value');
-        const max = progressBar.getAttribute('max');
-        return getPercentage(value, max);
-      }
-    }
-  };
-  
-  return (
-    <Fragment>
-      <h2>Q1 2019 goals</h2>
-      <StaticQuery
-        query={graphql`
-          query GoalsQuery {
-            allJson {
-              edges {
-                node {
-                  goalId
-                  title
-                  total
-                  completed
-                  quarter
-                }
+const GoalsComponent = () => (
+  <Fragment>
+    <h2>Q1 2019 goals</h2>
+    <StaticQuery
+      query={graphql`
+        query GoalsQuery {
+          allJson {
+            edges {
+              node {
+                goalId
+                title
+                total
+                completed
+                quarter
               }
             }
           }
-        `}
-        render={({ allJson: { edges: goals }}) =>
-          goals.map((goal, i) =>
-            <div className="goal__container"key={i}>
-              {goal.node.title}
-              <span className="goal__percentage">{getProgress(goal.node.goalId)}</span>
-              <progress max={goal.node.total} value={goal.node.completed} id={goal.node.goalId}></progress>
-            </div>
-          )
         }
-      />
-    </Fragment>
-  );
-};
+      `}
+      render={({ allJson: { edges: goals }}) =>
+        goals.map(({ node: goal }, i) =>
+          <div className="goal__container"key={i}>
+            {goal.title}
+            {goal.completed !== goal.total ? 
+              <span className="goal__percentage">{getPercentage(goal.completed, goal.total)}</span>
+              : <span className="goal__percentage goal__percentage--filled">100%</span>
+            }
+            <progress max="100" value={(goal.completed/goal.total)* 100} id={goal.goalId}></progress>
+          </div>
+        )
+      }
+    />
+  </Fragment>
+);
 
 export default GoalsComponent;
