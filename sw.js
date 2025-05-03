2d437c8292e483b1a1590a3a7ab8785368cb8c89
Cindy Haden <sceendy@gmail.com>
2025-05-03 17:56:49 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2d69fa6b157ff4c8098d.js"
+    "url": "webpack-runtime-189cec2bf6dd9e2faff0.js"
   },
   {
     "url": "styles.8bf4a43493364e356986.css"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d0a0177b2eea36a2bc414f9b1f203153"
+    "revision": "b74dc8fee9ccba13c83018aad1a3dc70"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ed71891e67857d7128bf.js"
